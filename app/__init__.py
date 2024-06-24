8f6384b2a085b2b91ad8be489d6ff059d1a2f778
hpnrep6 <hpnrep6@gmail.com>
2024-06-24 17:51:55 -0400

    Revert "[HTML]: Changed all generic data with my information"
    This reverts commit 03c006cc79429d847230a028a29f27d54fb2a670.

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ load_dotenv()
 app = Flask(__name__)
 
 # Variable declarations for texts
-title = "Carlos Martínez - MLH Fellow"
+title = "MLH Fellow"
 
-photo = "./static/img/me.jpeg"
+photo = "./static/img/logo.jpg"
 
 aboutMeGreetings = "Hello There! I'm Charlie"
 aboutMeDescription = "I'm a passionate and dedicated MLH Fellow specializing in Production Engineering. I thrive on solving complex problems, optimizing systems, and ensuring the reliability and scalability of web applications. With a strong foundation in software engineering principles and hands-on experience in deploying and managing production systems, I am committed to delivering high-quality, efficient solutions."
@@ -22,7 +22,7 @@ workExperience = [
         "company": "Major League Hacking",
         "date": "June 2024 - Present",
         "description": "Participated in a fellowship program focused on site reliability engineering and DevOps. Implemented CI/CD pipelines, monitored system performance, and collaborated with a global team to improve application scalability and reliability.",
-        "image": "./static/img/MLH_logo.png"
+        "image": "path/to/mlh_image.jpg"
     },
     {
         "id": 1,
@@ -30,7 +30,7 @@ workExperience = [
         "company": "KATCON",
         "date": "October 2023 - Present",
         "description": "Deployed web applications using React to optimize internal processes of customer service and billing. Integrated AWS services on my projects to manage databases, authentication, and cloud storage. Developed a software solution to manage financial processes and received positive feedback from company leadership.",
-        "image": "./static/img/katcon_logo.png"
+        "image": "path/to/katcon_image.jpg"
     },
     {
         "id": 2,
@@ -38,7 +38,7 @@ workExperience = [
         "company": "MedicFlow",
         "date": "December 2023 - April 2024",
         "description": "Developed the mobile application using React Native which is now the main product of the company. Led the Front-end development team and implemented Agile methodology to meet ambitious deadlines. Integrated AI models for speech recognition as well as LLMs for text processing.",
-        "image": "./static/img/MF_logo.jpeg"
+        "image": "path/to/medicflow_image.jpg"
     },
     {
         "id": 3,
@@ -46,7 +46,7 @@ workExperience = [
         "company": "Proyecto 99",
         "date": "December 2022 - February 2023",
         "description": "Developed a web application using React to manage their operations and allow students to sign up for courses. Learned about SCRUM methodology and software development processes. Deployed the project which is now used to manage student inscriptions.",
-        "image": "./static/img/p99_logo.png"
+        "image": "path/to/proyecto99_image.jpg"
     }
 
 ]
@@ -56,61 +56,60 @@ numOfJobs = len(workExperience)
 
 hobbies = Hobbies(hobbies = [
   Hobby(
-      title = "Playing Basketball with my friends",
-      description = "I've always enjoyed playing sports with my friends but for the past couple years I've been playing basketball with my friends every saturday and it's been a blast!",
+      title = "Underwater basket weaving",
+      description = "Every weekend, I weave baskets underwater",
       images = [
-
-          "./static/img/basketball.jpg"
+          "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
       ]
   ),
   Hobby(
-      title = "Playing the Piano",
-      description = "Whenever i have time i love to play the piano, I've been slowly self teaching myself and it's been a great experience. I love learining new pices and get better and better evrey time i try a new one",
+      title = "Underwater basket weaving 2",
+      description = "Every weekend, I weave baskets underwater",
       images = [
-          "./static/img/piano.jpg"
+          "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
       ]
   ),
   Hobby(
-      title = "Playing Video Games with my friends",
-      description = "I always enjoy hanging out with my friends but usualy we don'get to see each other that often outside of school because we live far away from each other. However, we always play video games together like Valorant or lethal company it's a great way to keep in touch and have fun!",
+      title = "Underwater basket weaving 3",
+      description = "Every weekend, I weave baskets underwater",
       images = [
-          "./static/img/steam.jpg",
+          "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
       ]
   )
 ])
 
 education = EducationHistory([
     EducationExperience(
-        title = "Bachelor in Computer Science",
-        school = "Tec de Monterrey",
-        description = ["4.0/4.0 GPA", "Specialized Courses: Data Structures and algorithms, Software Construction, Computer Networks, Computacional Methods "],
-        date = "2022 - 2026",
+        title = "Bachelor of Science in Computer Science",
+        school = "University of Toronto",
+        description = ["X/4.0 GPA", "Awards: Placeholder awards"],
+        date = "2021 - 2025",
         logo = "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Utoronto_coa.svg/1280px-Utoronto_coa.svg.png"
     ),
     EducationExperience(
-        title = "Multicultural High School Diploma",
-        school = "Prepa Tec",
-        description = ["3.7/4.0 GPA", "Specialized Courses: Advanced Calculus and Physics, French and introduction to Arduino"],
-        date = "2019 - 2022",
+        title = "Ontario Secondary School Degree (OSSD)",
+        school = "Some High School I guess",
+        description = ["X/4.0 GPA", "Awards: Placeholder awards"],
+        date = "2017 - 2021",
         logo = "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Utoronto_coa.svg/1280px-Utoronto_coa.svg.png"
     ),
 ])
 
 places_visited = PlacesVisited([
     PlaceVisited(
-        title = "Paris, France",
-        description = "A couple years ago i visited Paris with my family and it was super fun! We went to see the Eiffel Tower, Saint Chapelle and the castle of versailes",
-        url = """https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.148689953084!2d2.3423859766072286!3d48.855374971331806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1fd8767d47%3A0x33f441f9dc242768!2sSainte%20Chapelle!5e0!3m2!1ses-419!2smx!4v1719261874308!5m2!1ses-419!2smx"""
+        title = "Placeholder Place Visited",
+        description = "Last summer, I visited this place",
+        url = """https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Av.%20du%20Devonshire+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"""
     ),
     PlaceVisited(
-        title = "Rome, Italy",
-        description = "I also went to Rome for a week and it was amazing! I visited the Roman Coliseum, the fontana di trevi and a lot of beautiful churches that where several centuries old. I also went to the Roman Ruins and it was super intresting seeing all the ancient buildings still standing! My favorite part of Rome however was´t in Rome it was the Vatican where I saw the Sistine Chapel and the Basilica of Saint Peter. It was a super spiritual experience and I loved it!",
-        url = """https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d658.6749338764339!2d12.482714570913103!3d41.90084105214406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f6053278340d5%3A0xf676f1e1cc02bbb6!2sFontana%20di%20Trevi!5e0!3m2!1ses-419!2smx!4v1719262442457!5m2!1ses-419!2smx"""
+        title = "Placeholder Place Visited 2",
+        description = "Last summer, I visited this place 2",
+        url = """https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=niagara%20falls+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"""
     ),
     PlaceVisited(
-        title = "Florence, Italy",
-        description = "One of my favorite places in Europe is the Basilica de Santa Maria del Fiore, I went there with my familly and when we saw the facade we just stood there forever. The detail in this church and the whole building makes it an architectural wonder. I also went to several other churches and museums in Florence and it was a great experience!",
-        url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1347.7494125042347!2d11.25471911221566!3d43.77316970881967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132a5403bfe22ff5%3A0x5591438487aaf1f5!2sCatedral%20de%20Santa%20Mar%C3%ADa%20del%20Fiore!5e0!3m2!1ses-419!2smx!4v1719262814961!5m2!1ses-419!2smx"
+        title = "Placeholder Place Visited 3",
+        description = "Last summer, I visited this place 3",
+        url = "https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Av.%20du%20Devonshire+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
     )
 ])
 
--- app/static/js/glide.js
+++ app/static/js/glide.js
@@ -1,28 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let hobbySlider = document.querySelector('.hobby-slider');
+// let sliders = document.querySelectorAll(".image-slider")
 
-  if (hobbySlider) {
-    new Glide(hobbySlider, {
-      type: "carousel",
-      rewind: false,
+// for (let slider of sliders) {
+//   new Glide(slider, {
+//     rewind: false,
+//     perView: 1,
+//     focusAt: "center"
+//   }).mount()
+
+// }
+
+let hobbySlider = document.querySelector('.hobby-slider')
+
+new Glide(hobbySlider, {
+  type: "carousel",
+  rewind: false,
+  perView: 1,
+  focusAt: "center"
+}).mount()
+
+let visitedSlider = document.querySelector('.visited-slider')
+
+new Glide(visitedSlider, {
+  type: "carousel",
+  rewind: false,
+  perView: 2,
+  breakpoints: {
+    1800: {
       perView: 1,
       focusAt: "center"
-    }).mount();
+    }
   }
-
-  let visitedSlider = document.querySelector('.visited-slider');
-
-  if (visitedSlider) {
-    new Glide(visitedSlider, {
-      type: "carousel",
-      rewind: false,
-      perView: 2,
-      breakpoints: {
-        1800: {
-          perView: 1,
-          focusAt: "center"
-        }
-      }
-    }).mount();
-  }
-});
+}).mount()
