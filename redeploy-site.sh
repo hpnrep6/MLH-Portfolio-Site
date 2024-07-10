4e61406f91309eb3b5a97033c7239e173e003d43
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-07-10 14:34:05 -0600

    changed redeploy to work as a service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
 cd MLH-Portfolio-Site
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -d -s portfolio
-tmux send-keys -t portfolio 'flask run --host=0.0.0.0' C-m
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
