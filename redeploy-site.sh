88aa79657a2bfe82735f58a8cc14f6ca61fd4177
hpnrep6 <hpnrep6@gmail.com>
2024-07-12 17:11:06 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,7 @@
 tmux kill-session -t deployment
 git fetch && git reset origin/main --hard
-python3 -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-tmux new-session -d -s deployment 'flask run --host=0.0.0.0 --port=80'
+tmux new-session -d -s deployment 'python3 -m venv python3-virtualenv &&
+source python3-virtualenv/bin/activate &&
+pip install -r requirements.txt &&
+flask run --host=0.0.0.0 --port=80'
+echo 
