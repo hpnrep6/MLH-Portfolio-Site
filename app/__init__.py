973d98c2b599ae19803835ccb1f3692cab2a9a33
hpnrep6 <hpnrep6@gmail.com>
2024-07-12 17:23:52 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,37 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
 
 print(mydb)
 
+class TimelinePost(Model):
+  name = CharField()
+  email = CharField()
+  content = TextField()
+  created_at = DateTimeField(defualt=datetime.datetime.now)
+
+  class Meta:
+    database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+@app.route('/api/timeline_post', method=['POST'])
+def post_time_line_post():
+  name = request.form['name']
+  email = request.form['email']
+  content = request.form['content']
+  timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+  return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', method=['GET'])
+def get_time_line_post():
+  return {
+    'timeline_posts': [
+      model_to_dict(p)
+      for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+    ]
+  }
+
+
 # Variable declarations for texts
 title = "Young Chen"
 
