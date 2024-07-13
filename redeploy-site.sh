c0d83249bfef7049f5b369dff472afdb9b325856
hpnrep6 <hpnrep6@gmail.com>
2024-07-12 21:27:48 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,8 @@
 tmux kill-session -t deployment
 git fetch && git reset origin/main --hard
-tmux new-session -d -s deployment 'python3 -m venv python3-virtualenv &&
-source python3-virtualenv/bin/activate &&
-pip install -r requirements.txt &&
-flask run --host=0.0.0.0 --port=80'
+python3 -m venv python3-virtualenv 
+source python3-virtualenv/bin/activate 
+pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
 echo 'Deployment script executed'
