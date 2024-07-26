2a5b747c03277b56ed059d4ed1736028204f8277
hpnrep6 <hpnrep6@gmail.com>
2024-07-26 18:42:14 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,4 @@
-tmux kill-session -t deployment
 git fetch && git reset origin/main --hard
-python3 -m venv python3-virtualenv 
-source python3-virtualenv/bin/activate 
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
 echo 'Deployment script executed'
